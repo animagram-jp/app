4be6536484d527b32bfaa1bc5f0fab90b3e2a746
Andyou <andyou@animagram.jp>
2026-06-10 19:22:52 +0900

    u

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-// #![no_std]
 extern crate core;
 extern crate alloc;
+extern crate std;
 
 pub mod list;
 pub mod timestamp;
-// pub mod temporal;
 pub mod js_client;
 pub mod store;
 pub mod data_struct;
@@ -13,13 +12,14 @@ pub mod character;
 pub mod roll;
 pub mod event;
 
-macro_rules! debug_log {
-    ($($arg:tt)*) => {{
-        web_sys::console::error_1(
-            &wasm_bindgen::JsValue::from_str(&format!($($arg)*))
-        );
-    }};
-}
+// ============================================================
+// Global Allocator
+// ============================================================
+
+use dlmalloc::GlobalDlmalloc;
+
+#[global_allocator]
+static ALLOCATOR: GlobalDlmalloc = GlobalDlmalloc;
 
 // ============================================================
 // Lang
@@ -36,3 +36,31 @@ impl Lang {
         }
     }
 }
+
+// ============================================================
+// log
+// ============================================================
+
+macro_rules! debug_log {
+    ($($arg:tt)*) => {{
+        web_sys::console::error_1(
+            &wasm_bindgen::JsValue::from_str(&format!($($arg)*))
+        );
+    }};
+}
+
+// ============================================================
+// no_std (note)
+// ============================================================
+
+// #![no_std]
+// use core::{
+//     panic::Panicinfo,
+//     arch::wasm32::unreachable
+// };
+//
+// #[panic_handler]
+// fn panic(info: &PanicInfo) -> ! {
+//     debug_log!("panic: {}", info);
+//     unreachable()
+// }
