ed62ae223787054fd3f900eb9a2817c573a505f1
Andyou <andyou@animagram.jp>
2026-08-18 11:24:05 +0900

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,47 +1,45 @@
-const CACHE = "calendar-v1";
+const VERSION = "calendar-v1";
 const PRECACHE = [
-  "/",
-  "/init.js",
-  "/worker.js",
-  "/manifest.json",
-  "/icon.png",
-  "/app/app_bg.wasm",
-  "/app/app.js",
-  "manifest/state.yml",
-  "css/config.css",
-  "css/button.css",
-  "css/style.css",
-  "/fonts/IBMPlexSans-Regular.woff2",
-  "/fonts/IBMPlexSans-SemiBold.woff2",
+    "./",
+    "./init.js",
+    "./worker.js",
+    "./manifest.json",
+    "./icon.png",
+    "./app/app_bg.wasm",
+    "./app/app.js",
+    "./css/config.css",
+    "./css/button.css",
+    "./css/style.css",
+    "./font/IBMPlexSans-Regular.woff2",
+    "./font/IBMPlexSans-SemiBold.woff2",
 ];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then(c =>
-      Promise.allSettled(PRECACHE.map(url => c.add(url)))
-    ).then(() => self.skipWaiting())
-  );
+    e.waitUntil(
+        caches.open(VERSION).then(c =>
+            Promise.allSettled(PRECACHE.map(url => c.add(url)))
+        ).then(() => self.skipWaiting())
+    );
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+    e.waitUntil(
+        caches.keys()
+        .then(keys => Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k))))
+        .then(() => self.clients.claim())
+    );
 });
 
 self.addEventListener("fetch", (e) => {
-  if (e.request.method !== "GET") return;
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        if (res.ok) {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-        }
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
+    if (e.request.method !== "GET") return;
+    e.respondWith(
+        fetch(e.request)
+        .then(res => {
+            if (res.ok) {
+                const clone = res.clone();
+                caches.open(VERSION).then(c => c.put(e.request, clone));
+            }
+            return res;
+        }).catch(() => caches.match(e.request))
+    );
 });
